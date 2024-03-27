0c11f3385450ecb1ef2fe59b5e03681a1bff08b9
Earthbert <daraban_timotei@tutanota.com>
2024-03-27 18:48:10 +0200

    Implement ThreadPool using ThreadPoolExecutor
    Signed-off-by: Earthbert <daraban_timotei@tutanota.com>

--- app/task_runner.py
+++ app/task_runner.py
@@ -1,28 +1,57 @@
-from queue import Queue
-from threading import Thread, Event
+from threading import Thread, Lock
+from concurrent.futures import ThreadPoolExecutor
 import time
+import os
+
+CLEANUP_INTERVAL = 2
 
 class ThreadPool:
     def __init__(self):
-        # You must implement a ThreadPool of TaskRunners
-        # Your ThreadPool should check if an environment variable TP_NUM_OF_THREADS is defined
-        # If the env var is defined, that is the number of threads to be used by the thread pool
-        # Otherwise, you are to use what the hardware concurrency allows
-        # You are free to write your implementation as you see fit, but
-        # You must NOT:
-        #   * create more threads than the hardware concurrency allows
-        #   * recreate threads for each task
-        pass
+        # Check if TP_NUM_OF_THREADS environment variable is defined
+        if 'TP_NUM_OF_THREADS' in os.environ:
+            num_threads = int(os.environ['TP_NUM_OF_THREADS'])
+        else:
+            # Use hardware concurrency if TP_NUM_OF_THREADS is not defined
+            num_threads = os.cpu_count()
 
-class TaskRunner(Thread):
-    def __init__(self):
-        # TODO: init necessary data structures
-        pass
+        # Create a ThreadPoolExecutor with the specified number of threads
+        self.executor : ThreadPoolExecutor = ThreadPoolExecutor(max_workers=num_threads)
+        self.futures : dict = {}
+        self.dict_lock : Lock = Lock()
+        
+        self.cleaner = ThreadPoolCleaner(self)
+        self.cleaner.start()
+        
+    def submit(self, fn, *args, **kwargs):
+        future = self.executor.submit(fn, *args, **kwargs)
+        with self.dict_lock:
+            self.futures[future] = time.time()
+            
+        return future
+    
+    def check_job(self, job_id):
+        with self.dict_lock:
+            if self.futures[job_id].done():
+                return True
+        return False
+    
+    def shutdown(self):
+        self.executor.shutdown()
+        self.cleaner.join()
+
+
+class ThreadPoolCleaner(Thread):
+    def __init__(self, thread_pool: ThreadPool):
+        super().__init__()
+        self.thread_pool = thread_pool
 
     def run(self):
         while True:
-            # TODO
-            # Get pending job
-            # Execute the job and save the result to disk
-            # Repeat until graceful_shutdown
-            pass
+            time.sleep(CLEANUP_INTERVAL)
+            self.cleanup_futures()
+            
+    def cleanup_futures(self):
+        with self.thread_pool.dict_lock:
+            for future in list(self.thread_pool.futures.keys()):
+                if future.done():
+                    self.thread_pool.futures.pop(future)
