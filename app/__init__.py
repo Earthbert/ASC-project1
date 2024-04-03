153660971b29e59c88491575cb26abc653dfc820
Earthbert <daraban_timotei@tutanota.com>
2024-04-03 11:32:04 +0300

    Change logging format
    Signed-off-by: Earthbert <daraban_timotei@tutanota.com>

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,26 @@
+import time
 from flask import Flask
 from app.data_ingestor import DataIngestor
 from app.task_runner import ThreadPool
-import logging
+import logging, logging.handlers
 
-# Set up logging
-logging.basicConfig(filename='webserver.log', filemode='w', level=logging.INFO)
-werkzeug_logger = logging.getLogger('werkzeug')
 # Disable werkzeug logs
+werkzeug_logger = logging.getLogger('werkzeug')
 werkzeug_logger.setLevel(logging.ERROR)
 
 # Initialize the webserver
 webserver = Flask(__name__)
 webserver.tasks_runner = ThreadPool()
+
+# Set up root logger
+logging.basicConfig(level=logging.INFO, filename='webserver.log')
+
+logger = logging.getLogger(None)
+handler = logging.handlers.RotatingFileHandler("webserver.log", maxBytes=100000, backupCount=20)
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+formatter.converter = time.gmtime
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 # Initialize the data ingestor
 webserver.data_ingestor = DataIngestor("./nutrition_activity_obesity_usa_subset.csv")
